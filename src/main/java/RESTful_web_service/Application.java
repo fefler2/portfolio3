b48e1240f80f022693f0fafc711c396f2be917ef
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-23 15:21:37 +0200

    spring boot nie dziala mvn package

--- src/main/java/RESTful_web_service/Application.java
+++ src/main/java/RESTful_web_service/Application.java
@@ -19,17 +19,5 @@ public class Application {
         SpringApplication.run(Application.class);
     }
 //
-//    @Bean
-//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//        return builder.build();
-//    }
-//
-//    @Bean
-//    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-//        return args -> {
-//            Quote quote = restTemplate.getForObject(
-//                    "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-//            log.info(quote.toString());
-//        };
-//    }
+
 }
--- src/main/java/demo/Models/Rate.java
+++ src/main/java/demo/Models/Rate.java
@@ -13,6 +13,7 @@ import java.util.Map;
         "bid",
         "ask"
 })
+
 public class Rate {
 
     @JsonProperty("no")
